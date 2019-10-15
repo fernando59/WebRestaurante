8904c5ee503307641b7a38764368734ab11016dc
fernando59 <40122608+fernando59@users.noreply.github.com>
2019-10-15 12:50:30 -0400

    Aumentado carpetas de service y models

---
+++ src/app/models/persona.ts
@@ -0,0 +1,13 @@
+export class Persona{
+    constructor(
+        public nombre:string,
+        public apellido:string,
+        public edad:Number,
+        public telefono:string,
+        public direccion:string,
+        public ci:string,
+        public tipo:string,
+        public tipo_persona:Number
+
+    ){}
+}
